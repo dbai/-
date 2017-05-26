0b8191e793bfe7e3bf84911cc7b8a919e6581eda
Luke Pai <lukepai@benjaminde-imac.local>
2017-05-26 21:39:29 +0800

    Initial Commit

---
+++ 影音功能/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  影音功能
+//
+//  Created by Luke Pai on 2017/5/26.
+//  Copyright © 2017年 Luke Pai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
